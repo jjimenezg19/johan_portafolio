f1460874aa691491ecc050c4ea190662f0925f15
jjimenezg19 <47197113+jjimenezg19@users.noreply.github.com>
2024-09-27 16:41:34 -0500

    adding project items

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,18 +1,41 @@
-import { Col } from "react-bootstrap";
+import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { Carousel } from 'react-bootstrap'; // Importa el componente Carousel
 
-export const ProjectCard = ({ title, description, imgUrl, linkUrl }) => {
-  return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt={title} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-          <a href={linkUrl} target="_blank" rel="noopener noreferrer">
-            <button className="project-card-btn">See website</button>
-          </a>
+export const ProjectCard = ({ title, description, imgUrls }) => {
+    return (
+        <div className="project-card">
+            <div>
+                {/* Carousel de imágenes */}
+                <Carousel>
+                    {imgUrls.map((imgUrl, index) => (
+                        <Carousel.Item key={index}>
+                            <img
+                                className="place-self-center rounded-tl-lg rounded-tr-lg"
+                                src={imgUrl}
+                                alt={`${title} image ${index + 1}`}
+                            />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                
+                <div className="px-7 pt-7">
+                    <h4 className="text-h4-sm lg:text-h4-md xl:text-h4-lg font-kufam mb-2 lg:mb-4 xl:mb-6">
+                        {title}
+                    </h4>
+                    <p className="text-p font-league-spartan opacity-75">
+                        {description}
+                    </p>
+                </div>
+            </div>
+            <div className="flex p-7 mt-10 items-center justify-center">
+                <a
+                    href="#"
+                    className="project-card-btn"
+                >
+                    See website
+                </a>
+            </div>
         </div>
-      </div>
-    </Col>
-  );
+    );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
+import transitIgm1 from "../assets/img/TransitSolution1.png";
+import transitIgm2 from "../assets/img/TransitSolution2.png";
+import transitIgm3 from "../assets/img/TransitSolution3.png";
+import transitIgm4 from "../assets/img/TransitSolution4.png";
+import transitIgm5 from "../assets/img/TransitSolution5.png";
+import reactIcon from "../assets/img/react.svg";
+
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -13,19 +20,19 @@ export const Projects = () => {
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg1,
+      imgUrls: [projImg1, projImg2], // Pasar múltiples imágenes
+    },
+    {
+      title: "Latino Transit Solutions",
+      description: "This package shipment management platform allows registration of transports, routes, route approval, package shipment with associated transport, and route tracking",
+      imgUrls: [transitIgm1, transitIgm2, transitIgm3, transitIgm4, transitIgm5], 
+      img: reactIcon,// Múltiples imágenes
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg2,
+      imgUrls: [projImg3, projImg1], // Múltiples imágenes
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    
   ];
 
   return (
@@ -42,18 +49,13 @@ export const Projects = () => {
                   
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
+                    <Row xs={1} sm={2} md={3} className="g-4">
+                      {projects.map((project, index) => (
+                        <Col key={index}>
+                          <ProjectCard {...project} />
+                        </Col>
+                      ))}
+                    </Row>
                     </Tab.Pane>
                     
                   </Tab.Content>
@@ -63,7 +65,6 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-   
     </section>
-  )
-}
+  );
+};
